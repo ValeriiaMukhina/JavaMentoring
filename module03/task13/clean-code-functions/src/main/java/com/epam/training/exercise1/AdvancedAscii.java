75767abc7ecace86f85151afcd21c525c237f3cb
Valeriia_Biruk <Valeriia_Biruk@epam.com>
2018-11-12 18:12:23 +0200

    task 13 WIP

--- module03/task13/clean-code-functions/src/main/java/com/epam/training/exercise1/AdvancedAscii.java
+++ module03/task13/clean-code-functions/src/main/java/com/epam/training/exercise1/AdvancedAscii.java
@@ -6,17 +6,26 @@ package com.epam.training.exercise1;
  * @author Valeriia Biruk
  */
 public class AdvancedAscii {
+    private static final int INITIAL_MAX = 0;
+    private static final int INITIAL_MIN = 255 * 3;
+    private static final int STEP_RESOLUTION_Y = 60;
+    private static final int STEP_RESOLUTION_X = 200;
+    private static final char[] CHARS_BY_DARKNESS = {'#', '@', 'X', 'L', 'I', ':', '.', ' '};
 
     private Image image;
-    private static final char[] charsByDarkness = {'#', '@', 'X', 'L', 'I', ':', '.', ' '};
-    private int max = 0;
-    private int min = 255 * 3;
-    private int stepY = image.getHeight() / 60;
-    private int stepX = image.getWidth() / 200;
+    private int max;
+    private int min;
+    private int stepY;
+    private int stepX;
 
     public AdvancedAscii(Image image) {
         this.image = image;
+        max = INITIAL_MAX;
+        min = INITIAL_MIN;
+        stepY = image.getHeight() / STEP_RESOLUTION_Y;
+        stepX = image.getWidth() / STEP_RESOLUTION_X;
     }
+
     /**
      * main method to start converter.
      *
@@ -45,7 +54,7 @@ public class AdvancedAscii {
         for (int y = 0; y < image.getHeight(); y += stepY) {
             for (int x = 0; x < image.getWidth(); x += stepX) {
                 int intensity = image.getIntensity(new Point(x, y));
-                System.out.print(charsByDarkness[(intensity - min) * charsByDarkness.length / (max - min + 1)]);
+                System.out.print(CHARS_BY_DARKNESS[(intensity - min) * CHARS_BY_DARKNESS.length / (max - min + 1)]);
             }
             System.out.println();
         }
