0f0f0cd6c4783c6808acdbfd3eb12b76ddb41a1b
Valeriia Biruk <mukhinavaleria@gmail.com>
2018-11-19 22:13:03 +0200

    jenkinsfile modified

--- module02/task06/task06part2/src/main/java/sports/App.java
+++ module02/task06/task06part2/src/main/java/sports/App.java
@@ -8,7 +8,7 @@ import sports.runner.AppRunner;
  * @version 1.0
  */
 public class App {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         new AppRunner().start();
-    }
-}
+        }
+        }
