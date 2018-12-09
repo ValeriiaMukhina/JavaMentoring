38a6bfce4d12b6d8960358c9e16aca4f555c226e
Valeriia Biruk <mukhinavaleria@gmail.com>
2018-12-09 22:49:41 +0200

    task17 wip

--- module04/task17/servlet-with-spring-example/src/main/java/com/epam/trainings/servletwithspring/Main.java
+++ module04/task17/servlet-with-spring-example/src/main/java/com/epam/trainings/servletwithspring/Main.java
@@ -3,23 +3,14 @@ package com.epam.training;
 
 import java.util.Optional;
 
-import com.epam.training.config.AppConfig;
 import org.apache.catalina.startup.Tomcat;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Main {
     
     public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
     public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
     
     public static void main(String[] args) throws Exception {
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-
         String contextPath = "/" ;
         String appBase = ".";
         Tomcat tomcat = new Tomcat();   
